413274f191977e2c2f6711a8d8e26910bef2d3e9
Lê Đức Năng <53784923+hirazy2001@users.noreply.github.com>
2021-11-14 14:48:12 +0700

    update server

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.use(express.static('public'))
 app.use(bodyParser.json())
 
 let db = null;
-let collection = null;
-let allQuestions = {}
+let collectionQuestions = null;
+let collectionAttempts = null;
 
 app.on('listening', () => {
     console.log("Listening")
@@ -20,19 +20,13 @@ app.post('/attempts', getQuestions)
 async function getQuestions(req, res) {
 
     let ans = null
-    const o = await collection.find({}, {}).toArray((err, result) => {
+    const o = await collectionQuestions.find({}, {}).toArray(async(err, result) => {
         if (err) throw err;
 
         let ans = []
-
         let tmp = result
-
-        // Create id for each user
-        const id1 = new ObjectId();
-        const _id = id1.toString()
-        console.log("ID: " + _id)
-
         let questions = []
+        let correctAnswers = {}
 
         // Random 10 questions
         for (let i = 0; i < 10; i++) {
@@ -49,6 +43,9 @@ async function getQuestions(req, res) {
             // Push to source of array 
             ans.push(tmp[rand])
 
+            // Push to correct Answers
+            correctAnswers[tmp[rand]._id] = tmp[rand].correctAnswer
+
             // Remove Item which is selected
             tmp.splice(rand, 1)
         }
@@ -56,95 +53,153 @@ async function getQuestions(req, res) {
         const date = new Date()
         const time = date.toISOString()
 
-        const quiz = {
+        let attempts = {
             "questions": questions,
+            "correctAnswers": correctAnswers,
             "completed": false,
-            "score": 0,
-            "_id": _id,
             "startedAt": time,
-            "__v": 0
+            "answers": {},
+            "score": 0
         }
 
-        /**
-         * Save data to global variable of server
-         */
-        allQuestions[_id] = ans
+        let createAttempts = await collectionAttempts.insertOne(attempts)
 
-        res.status(201).json(quiz)
+        if (createAttempts) {
+
+            const quiz = {
+                "questions": questions,
+                "completed": false,
+                "score": 0,
+                "_id": createAttempts.insertedId,
+                "startedAt": time,
+                "__v": 0
+            }
+
+            res.status(201).json(quiz)
+        }
     })
 }
 
-app.post('/attempts/:id/submit', (req, res) => {
+app.post('/attempts/:id/submit', async(req, res) => {
+
     const _id = req.params.id
 
-    let answers = null
-    // Get answer from user
-    if (req.body.answers != undefined) {
-        answers = req.body.answers
-    }
-    else {
-        answers = JSON.parse(req.body)
-    }
+    console.log("ID: " + _id)
 
-    console.log("answers " + answers)
+    let attemptsAns = await collectionAttempts.findOne({ _id: ObjectId(_id) })
+        .then((attempt) => {
 
-    if (allQuestions[_id] != null) {
-        let score = 0
+            console.log("ID: " + attempt._id)
 
-        const questions = allQuestions[_id]
-
-        console.log("Question: " + answers[questions[0]._id])
-
-        // Check answer of user
-        for (let i = 0; i < questions.length; i++) {
-            if (answers[questions[i]._id] != null) {
-                if (parseInt(answers[questions[i]._id]) === questions[i].correctAnswer) {
-                    score += 1
-                }
+            let answers = null
+                // Get answer from user
+            if (req.body.answers != undefined) {
+                answers = req.body.answers
+            } else {
+                answers = JSON.parse(req.body)
             }
-        }
 
-        // Create correct answers for user
-        let correctAnswers = {}
+            console.log("Attempt " + attempt.completed)
 
-        for (let i = 0; i < questions.length; i++) {
-            correctAnswers[questions[i]._id] = questions[i].correctAnswer
-        }
+            // Completed
+            if (attempt.completed === true) {
 
-        let scoreText = "Practice more to improve it :D"
+                console.log("Completed")
 
-        if (score >= 5 && score < 7) {
-            scoreText = "Good, keep up!"
-        }
-        else if (score >= 7 && score < 9) {
-            scoreText = "Well done!"
-        }
-        else if (score >= 9 && score <= 10) {
-            scoreText = "Perfect!!"
-        }
+                const response = {
+                    "questions": attempt.questions,
+                    "completed": true,
+                    "score": attempt.score,
+                    "_id": _id,
+                    "correctAnswers": attempt.correctAnswers,
+                    "startedAt": attempt.startedAt,
+                    "_v": 0,
+                    "answers": attempt.answers,
+                    "scoreText": convertScoreText(attempt.score)
+                }
+                res.status(200).json(response)
 
-        const date = new Date()
-        const time = date.toISOString()
+            } else {
 
-        const response = {
-            "questions": questions,
-            "completed": true,
-            "score": score,
-            "_id": _id,
-            "correctAnswers": correctAnswers,
-            "startedAt": time,
-            "_v": 0,
-            "answers": answers,
-            "scoreText": scoreText
-        }
-        res.status(200).json(response)
-    }
+                console.log("Not Completed")
+
+                let score = 0
+
+                const questions = attempt.questions
+
+                console.log(questions.length)
+
+                //Init CorrectAttempts
+                let correctAttempts = attempt.correctAnswers;
+
+                // Check answer of user
+                for (let i = 0; i < questions.length; i++) {
+                    if (answers[questions[i]._id] != null) {
+                        if (parseInt(answers[questions[i]._id]) === correctAttempts[questions[i]._id]) {
+                            score += 1
+                        }
+                    }
+                }
+
+                console.log("scoreText " + correctAttempts)
+
+                // Create correct answers for user
+                let correctAnswers = {}
+
+                for (let i = 0; i < questions.length; i++) {
+                    correctAnswers[questions[i]._id] = correctAttempts[questions[i]._id]
+                }
+
+                let scoreText = convertScoreText(score)
+
+                const response = {
+                    "questions": questions,
+                    "completed": true,
+                    "score": score,
+                    "_id": _id,
+                    "correctAnswers": correctAnswers,
+                    "startedAt": attempt.startedAt,
+                    "_v": 0,
+                    "answers": answers,
+                    "scoreText": scoreText
+                }
+
+                res.status(200).json(response)
+
+                // Update attempts
+                let updateAttempt = collectionAttempts.updateOne({ _id: attempt._id }, {
+                    $set: {
+                        "completed": true,
+                        "score": score,
+                        "answers": answers
+                    }
+                })
+            }
+        })
+        .catch((err) => {
+            res.status(400).json()
+        })
 })
+
+function convertScoreText(score) {
+    let scoreText = "Practice more to improve it :D"
+
+    if (score >= 5 && score < 7) {
+        scoreText = "Good, keep up!"
+    } else if (score >= 7 && score < 9) {
+        scoreText = "Well done!"
+    } else if (score >= 9 && score <= 10) {
+        scoreText = "Perfect!!"
+    }
+
+    return scoreText
+}
 
 async function startServer() {
     const client = mongoDB.MongoClient.connect("mongodb://localhost:27017/wpr-quiz")
     db = (await client).db()
-    collection = db.collection('questions')
+    collectionQuestions = db.collection('questions')
+    collectionAttempts = db.collection('attempts')
 }
 
 app.listen(3000, () => {
